eff8d8a6c98e4027ae77ebc74c5d982d5aede45f
Florian Dunz <dunz.florian@web.de>
2021-06-13 21:18:47 +0200

    gmtk 2021 entry

--- GMTK2021/Source/GMTK2021/GMTK2021.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GMTK2021.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GMTK2021, "GMTK2021" );
-
-DEFINE_LOG_CATEGORY(LogGMTK2021)
- 
---
+++ SlimeShine/Source/SlimeShine/SlimeShineGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlimeShineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLIMESHINE_API ASlimeShineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
